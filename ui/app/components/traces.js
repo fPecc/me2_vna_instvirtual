101a0e7650e2753ff2b9cd72bd64076c80293548
fPecc <peccfederico@est.frba.utn.edu.ar>
2019-10-24 20:47:28 -0300

    Update traces.js

--- ui/app/components/traces.js
+++ ui/app/components/traces.js
@@ -20,7 +20,7 @@ app.controller("TracesCtrl", ['$scope','$http', function($scope, $http) {
 
     var req = {
         method: 'GET',
-        url: 'http://192.168.43.31:5000/api/getActualConfig'
+        url: 'http://127.0.0.1:5000/api/getActualConfig'
     };
     $http(req)
             .then(function(response) {
